428f5677fac8a49be5e9c9fd07b379e0eeecdb7e
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-10-24 20:39:02 +0300

    find requitements.txt

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JValtteri/mcp23017",
-    packages=setuptools.find_packages(),
+    #packages=setuptools.find_packages(),
+    packages=parse_requirements('requirements.txt', session='hack')
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
