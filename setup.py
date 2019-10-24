75229302fcac0d77cf92a6c8ab53a3fa98e02e88
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-10-24 20:43:02 +0300

    fix typo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JValtteri/mcp23017",
-    packages=setuptools.find_packages('RPi.GPIO', 'smbus2'),
+    packages=['RPi.GPIO', 'smbus2'],
     #packages=parse_requirements('requirements.txt', session='hack')
     classifiers=[
         "Programming Language :: Python :: 3",
